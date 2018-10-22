94f7eaa8fa970bd915ac6c8295924563c15d44ed
Javier Torres <javierandrestorresreyes@gmail.com>
2018-10-22 16:28:52 -0500

    holi

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,32 +1,83 @@
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class MainController {
+public class MainController implements Initializable {
+	
+	public static final String[] areas = new String[] {"CAR", "CM", "DAR", "IPM", "MA", "OPD", "OPF", "OPM", "OPP", "OT", "PI", "PMC", "PP", "PPQA", "QPM",
+			"RD", "REQM", "RSKM","SAM","TS","VAL","VER"};
 
-    @FXML
-    private JFXComboBox<String> comboArea;
+	@FXML
+	private JFXComboBox<String> comboArea;
 
-    @FXML
-    private JFXComboBox<Integer> comboAct;
+	@FXML
+	private JFXComboBox<Integer> comboAct;
 
-    @FXML
-    private JFXComboBox<Integer> comboObj;
+	@FXML
+	private JFXComboBox<Integer> comboObj;
 
-    @FXML
-    private JFXButton btnAgregar;
+	@FXML
+	private JFXButton btnAgregar;
 
-    @FXML
-    private JFXTextField txtTexto;
+	@FXML
+	private JFXTextArea txtTexto;
 
-    @FXML
-    void agregar(ActionEvent event) {
+	@FXML
+	void agregar(ActionEvent event) {
+		String msg = "Área de proceso: "+comboArea.getSelectionModel().getSelectedItem()+"\n";
+		
+		int inicio = comboAct.getSelectionModel().getSelectedIndex();
+		int fin = comboObj.getSelectionModel().getSelectedIndex();
+		
+		String[] metas = new String[3];
+		metas[0] = "Achieve Specific Goals";
+		metas[1] = "Institutionalize a Managed Process";
+		metas[2] = "Institutionalize a Defined Process";
+		
+		msg+= "Para alcanzar el nivel de capacidad "+fin+", se deben cumplir las siguientes metas: ";
+				
+		for(int i = inicio; i < fin;i++) {
+			msg+= "GG "+(i+1)+", ";
+		}
+		
+		msg+= "que consisten en ";
+		
+		for(int i = inicio; i < fin;i++) {
+			msg+= metas[i]+", ";
+		}
+		
+//		String[] info = new String[3];
+//		info[0] = 
+		
+		txtTexto.setText(msg);
+	}
 
-    }
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		ObservableList<Integer> listInt = FXCollections.observableArrayList(0, 1, 2, 3);
+		comboAct.setItems(listInt);
+		comboAct.getSelectionModel().select(0);
+
+		comboObj.setItems(listInt);
+		comboObj.getSelectionModel().select(0);
+
+		ObservableList<String> list = FXCollections.observableArrayList(areas);
+		comboArea.setItems(list);
+		comboArea.getSelectionModel().select(0);
+		
+		txtTexto.setWrapText(true);
+	}
 
 }
